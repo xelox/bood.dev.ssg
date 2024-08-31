0dac5f8e1a26bd19b3920d4ab93ab286105917df
xelox <alexdoppiaj@gmail.com>
2024-08-31 16:20:20 +0300

    LeafNode to only take 3 arguments now

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,12 +1,10 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, children=None, props=None):
-        super().__init__(tag, value, children, props)
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.children:
-            raise ValueError('LeafNode can not have children. sadge')
         if not self.value:
             raise ValueError('value is required on LeafNode.')
 
